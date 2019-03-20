de116f5bbff0af7ac3540d0d4da240705a456b07
inmikhaylov <qwerty@2>
2019-03-20 17:49:55 +0300

    Initial commit

---
+++ src/main/java/tacos/Order.java
@@ -0,0 +1,35 @@
+package tacos;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date placedAt;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToMany(targetEntity = Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
+    }
+}
